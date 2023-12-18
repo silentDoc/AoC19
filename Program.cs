2b22db19d0e0383c71a1a3b69011fcc26abcc741
silentDoc <ramon78t@yahoo.es>
2023-12-18 21:59:57 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace AoC19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 1;
+            int part = 1;
+            bool test = !false;
+
+            string input = "./Input/day" + day.ToString("00");
+            input += (test) ? "_test.txt" : ".txt";
+
+            Console.WriteLine("AoC 2019 - Day {0} , Part {1} - Test Data {2}", day, part, test);
+            Stopwatch st = new();
+            st.Start();
+            string result = day switch
+            {
+                1 => day1(input, part),
+                _ => throw new ArgumentException("Wrong day number - unimplemented")
+            };
+            st.Stop();
+            Console.WriteLine("Result : {0}", result);
+            Console.WriteLine("Elapsed : {0}", st.Elapsed.TotalSeconds);
+        }
+
+        static string day1(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            return "";
+        }
+
+    }
+}
