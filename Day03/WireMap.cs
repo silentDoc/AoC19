84b8c4dc99a5f45a3a61ee66799143d9193127a9
silentDoc <ramon78t@yahoo.es>
2023-12-20 23:22:36 +0100

    Day 3 - Part 2 Completed. Steps to a crossing

--- Day03/WireMap.cs
+++ Day03/WireMap.cs
@@ -1,14 +1,20 @@
 ﻿using AoC19.Common;
+using System.Runtime.ConstrainedExecution;
 
 namespace AoC19.Day03
 {
     internal class WireMap
     {
         Dictionary<Coord2D, int> Cells = new();
+        List<Coord2D> Wire1 = new();
+        List<Coord2D> Wire2 = new();
+
         Coord2D UP = (0, -1);
         Coord2D DOWN = (0, 1);
         Coord2D RIGHT = (1, 0);
         Coord2D LEFT = (-1, 0);
+
+        int wire = 1;
         
         void ParseLine(string line)
         {
@@ -33,8 +39,14 @@ namespace AoC19.Day03
                     if (!Cells.ContainsKey(current))
                         Cells[current] = 0;
                     Cells[current]++;
+
+                    if (wire == 1)
+                        Wire1.Add(current);
+                    else
+                        Wire2.Add(current);
                 }
             }
+            wire++;
         }
 
         public void ParseInput(List<string> lines)
@@ -43,7 +55,17 @@ namespace AoC19.Day03
         int DistToOrigin()
            => Cells.Keys.Where(x => Cells[x] > 1).Select(x => new Coord2D(0, 0).Manhattan(x)).Min();
 
+        int MinSteps()
+        {
+            var crossings = Cells.Keys.Where(x => Cells[x] > 1);
+            var w1Steps = crossings.Select(x => Wire1.IndexOf(x) + 1).ToList();
+            var w2Steps = crossings.Select(x => Wire2.IndexOf(x) + 1).ToList();
+            // There are crossings done by only 1 wire, we need to ignore positions of multiple
+            var dists = w1Steps.Zip(w2Steps, (f, s) => (f!=0 && s!=0) ? f + s : 999999).ToList();
+            return dists.Min();
+        }
+
         public int Solve(int part)
-            => DistToOrigin();
+            => part ==1 ? DistToOrigin() : MinSteps();
     }
 }
