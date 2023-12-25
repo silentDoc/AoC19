2134015f25a0cbeea1b579b1ac7b4f5762ac9ad2
silentDoc <ramon78t@yahoo.es>
2023-12-25 22:58:56 +0100

    Day 5 - Part 2 Completed. Now we jump

--- Day05/TestTerm.cs
+++ Day05/TestTerm.cs
@@ -4,6 +4,7 @@
     {
         Dictionary<int, int> IntCodes = new();
         int LastOutput = 0;
+        const int EXIT_PROGRAM = -9999999;
 
         public void ParseInput(List<string> lines)
         {
@@ -12,7 +13,7 @@
                 IntCodes[i] = nums[i];
         }
 
-        int RunOpCode(int Ptr)
+        int RunOpCode(int Ptr, int part =1)
         {
             var opCodeSet = IntCodes[Ptr];
             var strOpCode = opCodeSet.ToString("0000");
@@ -20,44 +21,69 @@
             var p1Mode = int.Parse(strOpCode.Substring(1, 1));
             var p2Mode = int.Parse(strOpCode.Substring(0, 1));
 
-            int v1 = IntCodes[Ptr + 1];
-            int v2 = IntCodes[Ptr + 2];
-            int v3 = IntCodes[Ptr + 3];
-
             if (opCode == 99)
-                return -1;
+                return EXIT_PROGRAM;
+
+            int v1 = IntCodes[Ptr + 1];
+            int v2 = opCode < 3 || opCode > 4 ? IntCodes[Ptr + 2] : 0;  // Just in case, we don't want to step off bounds
+            int v3 = opCode < 3 || opCode > 6 ? IntCodes[Ptr + 3] : 0;
 
             var op1 = p1Mode == 0 ? IntCodes[v1] : v1;
+            int op2;
 
-            var increment = -1;
+            var increment = 4;
             switch (opCode)
             {
-                case 1:
+                case 1:     // Sum / mul
                 case 2:
-                    var op2 = p2Mode == 0 ? IntCodes[v2] : v2;
-                    IntCodes[v3] = opCode == 1 ? op1 + op2
-                                               : op1 * op2;
-                    increment = 4;
+                    op2 = p2Mode == 0 ? IntCodes[v2] : v2;
+                    IntCodes[v3] = opCode == 1 ? op1 + op2 : op1 * op2;
                     break;
-                case 3:
-                    IntCodes[v1] = 1;
+                case 3:     // Input
+                    IntCodes[v1] = part == 1 ? 1 : 5;
                     increment = 2;
                     break;
-                case 4:
+                case 4:     // Output
                     LastOutput = op1;
                     increment = 2;
+                    break;
+                case 5:     // Jump if true - first param is non zero - we set the IntPtr to second param
+                    if (op1 != 0)
+                    {
+                        op2 = p2Mode == 0 ? IntCodes[v2] : v2;
+                        increment = op2 - Ptr;
+                    }
+                    else
+                        increment = 3;
+                    break;
+                case 6:     // Jump if false - first param is zero - we set the IntPtr to second param
+                    if (op1 == 0)
+                    {
+                        op2 = p2Mode == 0 ? IntCodes[v2] : v2;
+                        increment = op2 - Ptr;
+                    }
+                    else
+                        increment = 3;
+                    break;
+                case 7:     // Less than - first < second -- code[third] = 1 else 0
+                    op2 = p2Mode == 0 ? IntCodes[v2] : v2;
+                    IntCodes[v3] = (op1 < op2) ? 1 : 0;
+                    break;
+                case 8:     // Equals - - first == second -- code[third] = 1 else 0
+                    op2 = p2Mode == 0 ? IntCodes[v2] : v2;
+                    IntCodes[v3] = (op1 == op2) ? 1 : 0;
                     break;
             }
             return increment;
         }
 
-        int RunProgram()
+        int RunProgram(int part)
         {
             int Ptr = 0;
             while (IntCodes.Keys.Contains(Ptr))
             {
-                var increment = RunOpCode(Ptr);
-                if (increment == -1)
+                var increment = RunOpCode(Ptr, part);
+                if (increment == EXIT_PROGRAM)
                     break;
                 Ptr += increment;
 
@@ -66,6 +92,6 @@
         }
 
         public int Solve(int part)
-            => part == 1 ? RunProgram() : 0;
+            => RunProgram(part);
     }
 }
