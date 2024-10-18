031b7810d0718fefebea83867881f20933d9064c
silentDoc <ramon78t@yahoo.es>
2024-10-18 23:10:01 +0200

    Day 8 - Readme, main and input

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace AoC19
     {
         static void Main(string[] args)
         {
-            int day = 7;
-            int part = 2;
-            bool test = false;
+            int day = 8;
+            int part = 1;
+            bool test = true;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test.txt" : ".txt";
@@ -25,6 +25,7 @@ namespace AoC19
                 5 => day5(input, part),
                 6 => day6(input, part),
                 7 => day7(input, part),
+                8 => day8(input, part),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
@@ -88,6 +89,12 @@ namespace AoC19
             amp.ParseInput(lines);
 
             return amp.Solve(part).ToString();
+        }
+
+        static string day8(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            return "";
         }
 
     }
