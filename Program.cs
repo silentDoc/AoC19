50e8ad91ced69c85063ccf34ff8fa527a010482e
silentDoc <ramon78t@yahoo.es>
2024-10-30 22:07:42 +0100

    Day 17 - Readme, main and input

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace AoC19
     {
         static void Main(string[] args)
         {
-            int day = 16;
-            int part = 2;
-            bool test = false;
+            int day = 17;
+            int part = 1;
+            bool test = !false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -36,6 +36,7 @@ namespace AoC19
                 14 => day14(input, part),
                 15 => day15(input, part),
                 16 => day16(input, part),
+                17 => day17(input, part),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
@@ -174,6 +175,12 @@ namespace AoC19
             Day16.FFTCalculator fftCalculator = new();
             fftCalculator.ParseInput(lines);
             return fftCalculator.Solve(part).ToString();
+        }
+
+        static string day17(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            return "";
         }
     }
 }
